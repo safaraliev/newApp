721b00e1edd1ca204a8d734eb4892c32f465dc31
safaraliev <nurikonurik@gmail.com>
2025-02-06 03:01:45 -0600

    Initial Commit

---
+++ NewAPp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewAPp
+//
+//  Created by Нуридин Сафаралиев on 2/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
