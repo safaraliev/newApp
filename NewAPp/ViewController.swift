cf09d2174d0b3d40d859144f17844b90782a0f30
safaraliev <nurikonurik@gmail.com>
2025-02-06 03:38:30 -0600

    hahahah

--- NewAPp/ViewController.swift
+++ NewAPp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var fact = "Baia Loh"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //nurik huesos
-//        sam huesos suka
-//        haha
+        
+        
+//     fawfa
+//        wf
+//        awf
+//        aw
+//        fa
+//        wf
+//        aw
+//        f
+        
+        
     }
 
 
